724532c2bbd84074e865d4b335297ba9d0baeb81
Michael Bao <tcmb139@gmail.com>
2022-06-11 19:44:00 -0400

    got rid of border

--- src/components/GetUserStats/GetUserStats.js
+++ src/components/GetUserStats/GetUserStats.js
@@ -56,7 +56,7 @@ function GetUserStats(props) {
                     <UserVerdictGraph handle={props.handle} data={userStatus} />
                 </div>
             </div> */}
-            <div className="bg-neutral-900 border-2 shadow-md h-96">
+            <div className="bg-neutral-900 shadow-md h-96">
                 <UserRatingGraph handle={props.handle} data={userRating} />
             </div>
             {/* <div className="bg-neutral-900 shadow-md h-96 m-1 p-[1%]">
