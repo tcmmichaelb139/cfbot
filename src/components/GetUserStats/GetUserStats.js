d4a2a766c32272b7e5f0705e11c754ddb51d3385
Michael Bao <tcmb139@gmail.com>
2022-06-12 23:46:13 -0400

    added loading animation to the main page

--- src/components/GetUserStats/GetUserStats.js
+++ src/components/GetUserStats/GetUserStats.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+// loading
+import { HashLoader } from "react-spinners";
+
 // line charts
 import UserRatingChart from "./GetChart/UserRatingChart";
 import UserSolveCountChart from "./GetChart/UserSolveCountChart";
@@ -15,11 +18,14 @@ import NetworkError from "../Errors/NetworkError";
 import UserZeroContents from "../Errors/UserZeroContests";
 
 function GetUserStats(props) {
+    const [loading, setLoading] = useState(true);
+
     const [userRating, setUserRating] = useState();
     const [userStatus, setUserStatus] = useState();
     const [jsxCharts, setJsxCharts] = useState();
 
     useEffect(() => {
+        setLoading(true);
         axios
             .get(
                 "https://codeforces.com/api/user.rating?handle=" + props.handle
@@ -33,6 +39,7 @@ function GetUserStats(props) {
     }, [props]);
 
     useEffect(() => {
+        setLoading(true);
         axios
             .get(
                 "https://codeforces.com/api/user.status?handle=" + props.handle
@@ -46,6 +53,7 @@ function GetUserStats(props) {
     }, [props]);
 
     useEffect(() => {
+        setLoading(true);
         if (userRating === undefined || userStatus == undefined) null;
         else if (
             userRating === "ERR_BAD_RESPONSE" ||
@@ -60,19 +68,33 @@ function GetUserStats(props) {
         else if (userRating === "ERR_NETWORK" || userStatus === "ERR_NETWORK")
             setJsxCharts(<NetworkError />);
         else if (!userRating.length) setJsxCharts(<UserZeroContents />);
-        else
-            setJsxCharts(
-                <div className="bg-neutral-900 shadow-md h-96">
-                    <UserRatingChart handle={props.handle} data={userRating} />
-                    <UserSolveCountChart
-                        handle={props.handle}
-                        data={userStatus}
-                    />
-                </div>
-            );
+        else {
+            setTimeout(() => {
+                setJsxCharts(
+                    <div className="bg-neutral-900 shadow-md h-96">
+                        <UserRatingChart
+                            handle={props.handle}
+                            data={userRating}
+                        />
+                        <UserSolveCountChart
+                            handle={props.handle}
+                            data={userStatus}
+                        />
+                    </div>
+                );
+                setLoading(false);
+            }, 1000);
+        }
     }, [props, userRating, userStatus]);
 
-    return <div className="">{jsxCharts}</div>;
+    return (
+        <div className="">
+            <div className="absolute ml-[42.5%] w-20 h-20">
+                <HashLoader color="#10b981" loading={loading} size={75} />
+            </div>
+            {jsxCharts}
+        </div>
+    );
 }
 
 export default GetUserStats;
