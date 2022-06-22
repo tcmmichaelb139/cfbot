ffa5685b3f61b7ea14064619d62d1a51ae741d58
Michael Bao <tcmb139@gmail.com>
2022-06-22 14:49:35 -0400

    changed readme to org mode

--- src/components/GetProblem/GetProblem.js
+++ src/components/GetProblem/GetProblem.js
@@ -7,110 +7,107 @@ import UserNotFound from "../Errors/UserNotFound";
 import NetworkError from "../Errors/NetworkError";
 
 function GetProblem(props) {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const [problemsetProblems, setProblemsetProblems] = useState(
-        props.problemset
-    );
-    const [userStatus, setUserStatus] = useState();
-    const [problemRating, setProblemRating] = useState();
-    const [problemLink, setProblemLink] = useState();
+  const [problemsetProblems, setProblemsetProblems] = useState(
+    props.problemset
+  );
+  const [userStatus, setUserStatus] = useState();
+  const [problemRating, setProblemRating] = useState();
+  const [problemLink, setProblemLink] = useState();
 
-    useEffect(() => {
-        setLoading(true);
-        setProblemRating(props.rating);
-    }, [props.rating]);
+  useEffect(() => {
+    setLoading(true);
+    setProblemRating(props.rating);
+  }, [props.rating]);
 
-    useEffect(() => {
-        setLoading(true);
-        axios
-            .get(
-                "https://codeforces.com/api/user.status?handle=" + props.handle
-            )
-            .then((response) => {
-                setUserStatus(response.data.result);
-            })
-            .catch((error) => {
-                setUserStatus(error.code);
-            });
-    }, [props.handle]);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://codeforces.com/api/user.status?handle=" + props.handle)
+      .then((response) => {
+        setUserStatus(response.data.result);
+      })
+      .catch((error) => {
+        setUserStatus(error.code);
+      });
+  }, [props.handle]);
 
-    useEffect(() => {
-        setLoading(true);
-        if (problemsetProblems === undefined || userStatus == undefined) return;
-        if (
-            problemsetProblems === "ERR_BAD_RESPONSE" ||
-            userStatus == "ERR_BAD_RESPONSE"
-        )
-            return <ApiError />;
-        if (
-            problemsetProblems === "ERR_BAD_REQUEST" ||
-            userStatus == "ERR_BAD_REQUEST"
-        )
-            return <UserNotFound />;
-        if (problemsetProblems === "ERR_NETWORK" || userStatus == "ERR_NETWORK")
-            return <NetworkError />;
+  useEffect(() => {
+    setLoading(true);
+    if (problemsetProblems === undefined || userStatus == undefined) return;
+    if (
+      problemsetProblems === "ERR_BAD_RESPONSE" ||
+      userStatus == "ERR_BAD_RESPONSE"
+    )
+      return <ApiError />;
+    if (
+      problemsetProblems === "ERR_BAD_REQUEST" ||
+      userStatus == "ERR_BAD_REQUEST"
+    )
+      return <UserNotFound />;
+    if (problemsetProblems === "ERR_NETWORK" || userStatus == "ERR_NETWORK")
+      return <NetworkError />;
 
-        const tried = {};
+    const tried = {};
 
-        for (const problem of userStatus) {
-            if (tried[problem.contestId] == undefined)
-                tried[problem.contestId] = {};
-            if (tried[problem.contestId][problem.problem.index] == undefined)
-                tried[problem.contestId][problem.problem.index] = 1;
-        }
+    for (const problem of userStatus) {
+      if (tried[problem.contestId] == undefined) tried[problem.contestId] = {};
+      if (tried[problem.contestId][problem.problem.index] == undefined)
+        tried[problem.contestId][problem.problem.index] = 1;
+    }
 
-        // Knuth Shuffle
-        function shuffle(array) {
-            let currentIndex = array.length,
-                randomIndex;
+    // Knuth Shuffle
+    function shuffle(array) {
+      let currentIndex = array.length,
+        randomIndex;
 
-            // While there remain elements to shuffle.
-            while (currentIndex != 0) {
-                // Pick a remaining element.
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex--;
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
 
-                // And swap it with the current element.
-                [array[currentIndex], array[randomIndex]] = [
-                    array[randomIndex],
-                    array[currentIndex],
-                ];
-            }
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ];
+      }
 
-            return array;
-        }
+      return array;
+    }
 
-        const shuffledProblems = shuffle(problemsetProblems);
+    const shuffledProblems = shuffle(problemsetProblems);
 
-        for (let problem of shuffledProblems) {
-            if (
-                problem.rating == problemRating &&
-                (tried[problem.contestId] == undefined ||
-                    tried[problem.contestId] == undefined)
-            ) {
-                setProblemLink(
-                    "https://codeforces.com/problemset/problem/" +
-                        problem.contestId +
-                        "/" +
-                        problem.index
-                );
-                break;
-            }
-        }
-        setLoading(false);
-    }, [props, problemRating, problemsetProblems, userStatus]);
+    for (let problem of shuffledProblems) {
+      if (
+        problem.rating == problemRating &&
+        (tried[problem.contestId] == undefined ||
+          tried[problem.contestId] == undefined)
+      ) {
+        setProblemLink(
+          "https://codeforces.com/problemset/problem/" +
+            problem.contestId +
+            "/" +
+            problem.index
+        );
+        break;
+      }
+    }
+    setLoading(false);
+  }, [props, problemRating, problemsetProblems, userStatus]);
 
-    return (
-        <>
-            <div className="absolute right-0 top-12 w-20 h-20">
-                <HashLoader color="#10b981" loading={loading} size={50} />
-            </div>
-            <a href={problemLink} target="_blank" rel="noopener noreferrer">
-                Problem: {problemLink}
-            </a>
-        </>
-    );
+  return (
+    <>
+      <div className="absolute right-0 top-12 w-20 h-20">
+        <HashLoader color="#10b981" loading={loading} size={50} />
+      </div>
+      <a href={problemLink} target="_blank" rel="noopener noreferrer">
+        Problem: {problemLink}
+      </a>
+    </>
+  );
 }
 
 export default GetProblem;
--- src/components/GetUserStats/GetUserStats.js
+++ src/components/GetUserStats/GetUserStats.js
@@ -18,90 +18,77 @@ import NetworkError from "../Errors/NetworkError";
 import UserZeroContents from "../Errors/UserZeroContests";
 
 function GetUserStats(props) {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    const [userRating, setUserRating] = useState();
-    const [userStatus, setUserStatus] = useState();
-    const [jsxCharts, setJsxCharts] = useState();
+  const [userRating, setUserRating] = useState();
+  const [userStatus, setUserStatus] = useState();
+  const [jsxCharts, setJsxCharts] = useState();
 
-    useEffect(() => {
-        setLoading(true);
-        axios
-            .get(
-                "https://codeforces.com/api/user.rating?handle=" + props.handle
-            )
-            .then((response) => {
-                setUserRating(response.data.result);
-            })
-            .catch((error) => {
-                setUserRating(error.code);
-            });
-    }, [props]);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://codeforces.com/api/user.rating?handle=" + props.handle)
+      .then((response) => {
+        setUserRating(response.data.result);
+      })
+      .catch((error) => {
+        setUserRating(error.code);
+      });
+  }, [props]);
 
-    useEffect(() => {
-        setLoading(true);
-        axios
-            .get(
-                "https://codeforces.com/api/user.status?handle=" + props.handle
-            )
-            .then((response) => {
-                setUserStatus(response.data.result);
-            })
-            .catch((error) => {
-                setUserStatus(error.code);
-            });
-    }, [props]);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("https://codeforces.com/api/user.status?handle=" + props.handle)
+      .then((response) => {
+        setUserStatus(response.data.result);
+      })
+      .catch((error) => {
+        setUserStatus(error.code);
+      });
+  }, [props]);
 
-    useEffect(() => {
-        setLoading(true);
-        setTimeout(() => {
-            if (userRating === undefined || userStatus == undefined) null;
-            else if (
-                userRating === "ERR_BAD_RESPONSE" ||
-                userStatus === "ERR_BAD_RESPONSE"
-            )
-                setJsxCharts(<ApiError />);
-            else if (
-                userRating === "ERR_BAD_REQUEST" ||
-                userStatus === "ERR_BAD_REQUEST"
-            )
-                setJsxCharts(<UserNotFound />);
-            else if (
-                userRating === "ERR_NETWORK" ||
-                userStatus === "ERR_NETWORK"
-            )
-                setJsxCharts(<NetworkError />);
-            else if (!userRating.length) setJsxCharts(<UserZeroContents />);
-            else {
-                setJsxCharts(
-                    <div>
-                        <div className="bg-neutral-900 shadow-md h-96 mb-4">
-                            <UserRatingChart
-                                handle={props.handle}
-                                data={userRating}
-                            />
-                        </div>
-                        <div className="bg-neutral-900 shadow-md h-96 mb-4">
-                            <UserSolveCountChart
-                                handle={props.handle}
-                                data={userStatus}
-                            />
-                        </div>
-                    </div>
-                );
-            }
-            setLoading(false);
-        }, 1000);
-    }, [props, userRating, userStatus]);
-
-    return (
-        <div className="">
-            <div className="absolute right-0 top-12 w-20 h-20">
-                <HashLoader color="#10b981" loading={loading} size={50} />
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      if (userRating === undefined || userStatus == undefined) null;
+      else if (
+        userRating === "ERR_BAD_RESPONSE" ||
+        userStatus === "ERR_BAD_RESPONSE"
+      )
+        setJsxCharts(<ApiError />);
+      else if (
+        userRating === "ERR_BAD_REQUEST" ||
+        userStatus === "ERR_BAD_REQUEST"
+      )
+        setJsxCharts(<UserNotFound />);
+      else if (userRating === "ERR_NETWORK" || userStatus === "ERR_NETWORK")
+        setJsxCharts(<NetworkError />);
+      else if (!userRating.length) setJsxCharts(<UserZeroContents />);
+      else {
+        setJsxCharts(
+          <div>
+            <div className="bg-neutral-900 shadow-md h-96 mb-4">
+              <UserRatingChart handle={props.handle} data={userRating} />
             </div>
-            {jsxCharts}
-        </div>
-    );
+            <div className="bg-neutral-900 shadow-md h-96 mb-4">
+              <UserSolveCountChart handle={props.handle} data={userStatus} />
+            </div>
+          </div>
+        );
+      }
+      setLoading(false);
+    }, 1000);
+  }, [props, userRating, userStatus]);
+
+  return (
+    <div className="">
+      <div className="absolute right-0 top-12 w-20 h-20">
+        <HashLoader color="#10b981" loading={loading} size={50} />
+      </div>
+      {jsxCharts}
+    </div>
+  );
 }
 
 export default GetUserStats;
