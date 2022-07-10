2e35f54530424bacd75730b62e15e08c7a210bfc
Michael Bao <tcmb139@gmail.com>
2022-07-10 15:27:21 -0400

    text form

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,47 +7,47 @@ import GetUserStats from "../components/GetUserStats";
 import SubmitButton from "../components/Forms/SubmitButton";
 
 export default function Home() {
-    const userHandle = useRef();
-    const [jsxCharts, setJsxCharts] = useState();
+  const userHandle = useRef();
+  const [jsxCharts, setJsxCharts] = useState();
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
-        if (userHandle.current.value) {
-            setJsxCharts(<GetUserStats handle={userHandle.current.value} />);
-        }
-    };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (userHandle.current.value) {
+      setJsxCharts(<GetUserStats handle={userHandle.current.value} />);
+    }
+  };
 
-    return (
-        <>
-            <Head>
-                <meta
-                    name="description"
-                    content="Codeforces Bot for stats, problems, and more."
-                />
-                <meta name="og:url" content="https://cfbot.michaelbao.io" />
-                <meta name="og:title" content="Codeforces Bot" />
-                <meta
-                    name="og:description"
-                    content="Codeforces Bot for stats, problems, and more."
-                />
-            </Head>
+  return (
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Codeforces Bot for stats, problems, and more."
+        />
+        <meta name="og:url" content="https://cfbot.michaelbao.io" />
+        <meta name="og:title" content="Codeforces Bot" />
+        <meta
+          name="og:description"
+          content="Codeforces Bot for stats, problems, and more."
+        />
+      </Head>
 
-            <Header />
+      <Header />
 
-            <main className="ml-64">
-                <div className="m-4">
-                    <form onSubmit={onFormSubmit}>
-                        <input
-                            ref={userHandle}
-                            type="text"
-                            placeholder="Handle"
-                            className="bg-neutral-900 border-[1px] border-neutral-800 placeholder-neutral-600 rounded-md p-4 shadow-md"
-                        />
-                        <SubmitButton />
-                    </form>
-                    {jsxCharts}
-                </div>
-            </main>
-        </>
-    );
+      <main className="ml-64">
+        <div className="m-4">
+          <form onSubmit={onFormSubmit}>
+            <input
+              ref={userHandle}
+              type="text"
+              placeholder="Handle"
+              className="bg-neutral-900 outline-none focus:ring ring-neutral-500 ring-opacity-50 border border-neutral-800 placeholder-neutral-600 rounded-md p-4 m-4 shadow-md transition"
+            />
+            <SubmitButton />
+          </form>
+          {jsxCharts}
+        </div>
+      </main>
+    </>
+  );
 }
